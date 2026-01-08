305b690ba466d7e8e7289ddafee6143b18d98597
matus <matussadovsky99@gmail.com>
2026-01-08 18:16:56 +0100

    .

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import streamlit as st
-st.title("Pre inteligentných ľudí")
-with open("trieskove_obrabanie.odt", "rb") as file:
-    btn = st.download_button(
-            label="Download",
-            data=file,
-            file_name="trieskove_obrabanie.odt",
-            mime="application/vnd.oasis.opendocument.text",
-            key="1"
-          )
-st.title("Pre kognitívne menej vyspelých jedincov")
+
+st.title("Základná verzia")
 with open("trieskove_brabanie(2).odt", "rb") as file:
     buten = st.download_button(
             label="Download",
@@ -16,4 +8,13 @@ with open("trieskove_brabanie(2).odt", "rb") as file:
             file_name="trieskove_obrabanie.odt",
             mime="application/vnd.oasis.opendocument.text",
             key="2"
+          )
+st.title("Rozšírená verzia")
+with open("trieskove_obrabanie.odt", "rb") as file:
+    btn = st.download_button(
+            label="Download",
+            data=file,
+            file_name="trieskove_obrabanie.odt",
+            mime="application/vnd.oasis.opendocument.text",
+            key="1"
           )
