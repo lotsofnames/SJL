e24001485f15903b62ff850027f4f5d055a1478d
matus <matussadovsky99@gmail.com>
2026-01-12 17:02:49 +0100

    oprava chýb

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import streamlit as st
 
-st.title("Základná verzia")
-with open("trieskove_brabanie(2).odt", "rb") as file:
-    buten = st.download_button(
-            label="Download",
-            data=file,
-            file_name="trieskove_obrabanie.odt",
-            mime="application/vnd.oasis.opendocument.text",
-            key="2"
-          )
-st.title("Rozšírená verzia")
+st.title("Trieskové obrábanie - Poznámky")
 with open("trieskove_obrabanie.odt", "rb") as file:
     btn = st.download_button(
             label="Download",
